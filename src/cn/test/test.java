47a90c69d6ca429916ceb5be62f1bf82f270139f
SmileSkype <33144081+SmileSkype@users.noreply.github.com>
2017-10-31 18:17:51 +0800

    1
    1

--- src/cn/test/test.java
+++ src/cn/test/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
 		System.out.println("Hello GitHub 服务器");
+		System.out.println("我是服务器");
 	}
 
 }
